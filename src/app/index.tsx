20edaf4a1462b39d118183f12871c2fbeb80e7d7
Lin Lan <lanlin@meideng.net>
2021-08-21 19:48:23 +0800

    Step2

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,6 +4,8 @@ import classNames from 'classnames';
 import videoStart from '../../static/start.mp4';
 import videoMain from '../../static/main_bg.mp4';
 import imageLoading from '../../static/loading.gif';
+import imageMain1 from '../../static/main-1.png';
+import imageMain2 from '../../static/main-2.png';
 
 export function App(): JSX.Element {
   const [loading1, setLoading1] = React.useState(true);
@@ -32,32 +34,77 @@ export function App(): JSX.Element {
 
   return (
     <div className={styles.app}>
-      <div
-        className={classNames(styles.loadingContainer, loading1 || loading2 || styles.hide)}
-        style={{ backgroundImage: `url(${imageLoading})` }}
-      />
+      <div className={styles.header}>
+        <div className={styles.content}>
+          <span>
+            本网页仅供交流学习使用，视频、图片等资源归版权方所有，
+            <a href="https://www.bilibili.com/blackboard/activity-yellowVSgreen7th.html">原始网页链接</a>
+          </span>
+        </div>
+      </div>
 
-      <video
-        className={classNames(styles.videoStart, (loading1 || status1 === 2) && styles.hide)}
-        muted
-        preload="auto"
-        ref={ref1}
-        onLoadedData={handleLoad1}
-        onEnded={handleEnd1}
-      >
-        <source src={videoStart} type="video/mp4" />
-      </video>
+      <div className={styles.main}>
+        <div
+          className={classNames(styles.loadingContainer, loading1 || loading2 || styles.hide)}
+          style={{ backgroundImage: `url(${imageLoading})` }}
+        />
 
-      <video
-        className={classNames(styles.videoMain, status1 === 2 || styles.hide)}
-        muted
-        preload="auto"
-        loop
-        ref={ref2}
-        onLoadedData={handleLoad2}
-      >
-        <source src={videoMain} type="video/mp4" />
-      </video>
+        <video
+          className={classNames(styles.videoStart, (loading1 || status1 === 2) && styles.hide)}
+          muted
+          preload="auto"
+          ref={ref1}
+          onLoadedData={handleLoad1}
+          onEnded={handleEnd1}
+        >
+          <source src={videoStart} type="video/mp4" />
+        </video>
+
+        <video
+          className={classNames(styles.videoMain, status1 === 2 || styles.hide)}
+          muted
+          preload="auto"
+          loop
+          ref={ref2}
+          onLoadedData={handleLoad2}
+        >
+          <source src={videoMain} type="video/mp4" />
+        </video>
+
+        {loading2 || <Main visible={status1 === 2} />}
+      </div>
     </div>
+  );
+}
+
+function Main(props: { visible: boolean }): JSX.Element {
+  const { visible } = props;
+  return (
+    <div className={classNames(styles.main1, visible && styles.show)}>
+      <MainPage1 />
+    </div>
+  );
+}
+
+function MainPage1(): JSX.Element {
+  const ref = React.useRef<HTMLImageElement>();
+  // React.useEffect(() => {
+  //   const tween = gsap.to(ref.current, {
+  //     y: '2vh',
+  //     repeat: -1,
+  //     duration: 1,
+  //     ease: "power1.inOut",
+  //     yoyo: true,
+  //   });
+  //   return ()=>{
+  //     tween.kill()
+  //   }
+  // }, []);
+
+  return (
+    <>
+      <img src={imageMain1} className={styles.img1} alt="act-detail" />
+      <img src={imageMain2} className={styles.img2} alt="arrow" ref={ref} />
+    </>
   );
 }
