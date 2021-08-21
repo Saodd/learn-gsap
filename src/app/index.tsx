f6766b9029498e3891652eb024aa2007c8c92522
Lin Lan <lanlin@meideng.net>
2021-08-21 11:49:59 +0800

    React4

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,23 +7,19 @@ export function App(): JSX.Element {
     const appRef = React.useRef();
     const q = gsap.utils.selector(appRef)
 
-    const tlRef = React.useRef<gsap.core.Timeline>();
-
-    React.useEffect(() => {
-        tlRef.current = gsap.timeline()
-            .to(q("." + styles.box), {
-                scale: 1.5,
-            })
-    }, []);
     const handleEnter = React.useCallback(() => {
-        tlRef.current.reversed(true)
+        gsap.to(q("." + styles.box), {
+            scale: 1.5,
+        })
     }, [])
     const handleLeave = React.useCallback(() => {
-        tlRef.current.reversed(false)
+        gsap.to(q("." + styles.box), {
+            scale: 1,
+        })
     }, [])
 
     return (
-        <div className={styles.App} ref={appRef} >
+        <div className={styles.App} ref={appRef}>
             <div className={styles.box} onMouseEnter={handleEnter} onMouseLeave={handleLeave}>Box</div>
         </div>
     );
