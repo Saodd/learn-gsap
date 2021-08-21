07ad0209483e6db3b20ba196c55d6d5c32ec1d78
Lin Lan <lanlin@meideng.net>
2021-08-21 10:45:54 +0800

    React2

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,19 +2,36 @@ import * as React from 'react';
 import gsap from 'gsap'
 import styles from './index.scss'
 
+
+function Box(props: { children: React.ReactNode }) {
+    const {children} = props
+    return <div className={styles.box}>{children}</div>;
+}
+
+function Container() {
+    return <div><Box>Nested Box</Box></div>;
+}
+
+
 export function App(): JSX.Element {
-    const boxRef = React.useRef();
+    const appRef = React.useRef();
+    const q = gsap.utils.selector(appRef)
 
     React.useEffect(() => {
-        gsap.to(boxRef.current, {
-            duration: 5,
-            rotation: "+=360",
+        gsap.to(q("." + styles.box), {
+            x: 100,
+            stagger: 0.33,
+            repeat: -1,
+            repeatDelay: 1,
+            yoyo: true
         });
     });
 
     return (
-        <div className={styles.App}>
-            <div className={styles.box} ref={boxRef}>Hello</div>
+        <div className={styles.App} ref={appRef}>
+            <Box>Box1</Box>
+            <Container/>
+            <Box>Box2</Box>
         </div>
     );
 }
